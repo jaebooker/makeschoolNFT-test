f83b8922de391e4f7644fc8af1eaad25bdfb95ba
jaebooker <jaesonbooker@gmail.com>
2021-04-19 19:23:49 -0500

    improved

--- index.js
+++ index.js
@@ -8,16 +8,13 @@ const fetch = require('node-fetch');
 // const url = 'https://api.opensea.io/api/v1/events/?collection_slug=makecoin01';
 const url = 'https://api.opensea.io/api/v1/assets?collection=makecoin01';
 const options = {method: 'GET', qs: {order_direction: 'desc', offset: '0', limit: '20'}};
-
+let token_dict = {};
 fetch(url, options)
   .then(res => res.json())
   .then(json => {
     json.assets.forEach(element => {
-      if(element.num_sales == 0) {
-        console.log("blue!")
-      } else {
-        console.log("red!")
-      }
+      token_dict[element.token_id] = element.num_sales
+      console.log(token_dict)
     });
   })
   .catch(err => console.error('error:' + err));
